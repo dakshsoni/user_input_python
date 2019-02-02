a472b5c581fd2b5249033252375cdf27a6b32b0c
deepak soni <46437145+dakshsoni@users.noreply.github.com>
2019-02-02 06:17:30 +0000

    basic but useful example of dictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+>>> d1={'key':123,123:'key','value':'key'}
+>>> d1
+{123: 'key', 'value': 'key', 'key': 123}
+
+
+
+
+
+>>> d1={'key':123,123:'key','value':'key','key':909}
+>>> d1
+{123: 'key', 'value': 'key', 'key': 909}
